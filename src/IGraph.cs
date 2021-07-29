151a3eb5eae2bba7a68e296be94f45e9e8bcebe3
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-07-29 13:42:16 +0300

    Doc changes, moved to src folder

--- src/IGraph.cs
+++ src/IGraph.cs
@@ -1,22 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.ObjectModel;
-using System.Collections.Generic;
 
 namespace GraphLibrary {
+
     /// <summary>
     /// An interface defining all standard operations in any generalized graph.
     /// The graph holds a reference to any object for each internal vertex.<br></br>
     /// This way clients can store and get references to their objects
     /// while traversing the graph.
     /// </summary>
-    /// <typeparam name="VertexT">The type of objects stored in the graph's vertices.</typeparam>
+    /// <typeparam name="VertexT">The type of objects stored in the graph's vertices.
+    /// It's recommended it implements the <see cref="Object.ToString"/> method.</typeparam>
     /// <typeparam name="EdgeT">The type of objects stored in the graph's edges.</typeparam>
     public interface IGraph<VertexT, EdgeT> {
-
+        /// <summary>
+        /// How many vertices exist in the graph.
+        /// </summary>
         int Size {get;}
 
+        /// <summary>
+        /// Whether or not this is a directed graph.
+        /// </summary>
+        /// <returns>True if the graph is directed.</returns>
         bool IsDirected {get;}
 
+        /// <summary>
+        /// Whether or not there are any vertices in the graph.
+        /// </summary>
+        /// <returns>True if the graph is empty.</returns>
         bool IsEmpty();
 
         /// <summary>
@@ -29,7 +40,7 @@ namespace GraphLibrary {
         /// Removes a vertex from the graph.
         /// </summary>
         /// <param name="key">The object whose vertex needs to be removed.</param>
-        /// <exception cref="Exception">If there is no vertex with the object requested.</exception>
+        /// <exception cref="InvalidVertexException">If there is no vertex with the object requested.</exception>
         void RemoveVertex(VertexT key);
 
         /// <summary>
@@ -38,7 +49,8 @@ namespace GraphLibrary {
         /// </summary>
         /// <param name="obj1">The start of the newly created edge between the vertices</param>
         /// <param name="obj2">The end of the newly created edge between the vertices</param>
-        /// <param name="weight">The weight of the edge. The default weight is 1.</param>
+        /// <param name="value">The value of the edge.</param>
+        /// <exception cref="InvalidEdgeException">If there already exists a connection between the vertices.</exception>
         Edge<VertexT, EdgeT> Connect(VertexT obj1, VertexT obj2, EdgeT value);
 
         /// <summary>
@@ -46,14 +58,14 @@ namespace GraphLibrary {
         /// </summary>
         /// <param name="edge">The edge to be removedd</param>
         /// <returns>The content of the edge.</returns>
-        /// <exception cref="Exception">If thereis no such edge in the graph.</exception>
+        /// <exception cref="InvalidEdgeException">If there is no such edge in the graph.</exception>
         EdgeT Disconnect(Edge<VertexT, EdgeT> edge);
 
         /// <summary>
         /// Checks whether or not the vertices containing the two objects are connected. 
         /// </summary>
         /// <returns>True if connected, False otherwise</returns>
-        /// <exception cref="Exception">If any of the objects don't exist in the graph.</exception>
+        /// <exception cref="InvalidVertexException">If any of the objects don't exist in the graph.</exception>
         bool AreAdjacent(VertexT obj1, VertexT obj2);
 
         /// <summary>
@@ -61,7 +73,8 @@ namespace GraphLibrary {
         /// </summary>
         /// <param name="key">The vertex whose neighbours were requested.</param>
         /// <returns>A read-only collection of <see cref="VertexT"/>s starting from the given vertex and ending on all the neighbouring vertices.</returns>
-        ReadOnlyCollection<VertexT> IncidentEdges(VertexT key);
+        /// <exception cref="InvalidVertexException">If the vertex doesn't exist.</exception>
+        ReadOnlyCollection<Edge<VertexT, EdgeT>> IncidentEdges(VertexT key);
 
         /// <summary>
         /// Get all objects stored in the graph.
@@ -80,7 +93,7 @@ namespace GraphLibrary {
         /// </summary>
         /// <param name="oldValue">The object to be replaced</param>
         /// <param name="newValue">The new object</param>
-        /// <exception cref="VertexNonExistentException{VertexT}">If "oldValue" doesn't exist.</exception>
+        /// <exception cref="InvalidVertexException">If "oldValue" doesn't exist.</exception>
         void ReplaceVertex(VertexT oldValue, VertexT newValue);
 
         /// <summary>
@@ -89,7 +102,7 @@ namespace GraphLibrary {
         /// </summary>
         /// <param name="edge">The edge.</param>
         /// <param name="newValue">The value to replace the old one.</param>
-        /// <exception cref="EdgeNonExistentException{VertexT, EdgeT}">If the edge doesn't exist.</exception>
+        /// <exception cref="InvalidEdgeException">If the edge doesn't exist.</exception>
         void ReplaceEdge(Edge<VertexT, EdgeT> edge, EdgeT newValue);
     }
 }
