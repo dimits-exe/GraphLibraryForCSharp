50405e31456599060adbe37235c003b830795606
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-07-29 13:26:52 +0300

    Renamed exceptions

---
+++ src/InvalidEdgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GraphLibrary {
+    /// <summary>
+    /// An exception thrown to indicate an edge between two vertices doesn't exist in the specified graph.
+    /// </summary>
+    public class InvalidEdgeException : ArgumentException {
+
+        public InvalidEdgeException() : base() {}
+
+        public InvalidEdgeException(String error) : base(error) {}
+
+        public InvalidEdgeException(String error, Exception inner) : base(error, inner) {}
+
+    }
+}
---
+++ src/InvalidVertexException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GraphLibrary {
+    /// <summary>
+    /// An exception thrown to indicate a vertex doesn't exist in the specified graph.
+    /// </summary>
+    public class InvalidVertexException : ArgumentException {
+
+        public InvalidVertexException() : base() { }
+
+        public InvalidVertexException(String error) : base(error) {}
+
+        public InvalidVertexException(String error, Exception inner) : base(error, inner) {}
+
+    }
+
+}
