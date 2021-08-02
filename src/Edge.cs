b839f4852be330a7bed41de200e716f4c93e78f1
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-08-02 11:35:09 +0300

    Made serializable

--- src/Edge.cs
+++ src/Edge.cs
@@ -1,30 +1,43 @@
 using System;
-using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace GraphLibrary {
+    
     /// <summary>
     /// A simple struct encapsulating information about any edge in a <see cref="Graph{T}"/> instance.<br></br>
     /// Specifically holds the names of the nodes being connected by the edge and the edge's value.
     /// </summary>
     /// <typeparam name="VertexT">The type of the objects each of the two <see cref="Graph{VertexT,EdgeT}"/> vertices hold.</typeparam>
     /// <typeparam name="EdgeT">The type of the objects held in each edge.</typeparam>
+    [DataContract]
     public struct Edge<VertexT, EdgeT> {
-        private readonly VertexT startPoint;
-        private readonly VertexT endPoint;
+        private VertexT startPoint;
+        private VertexT endPoint;
         private EdgeT edgeValue;
 
+        [DataMember]
         public VertexT StartPoint {
             get {
                 return startPoint;
             }
+            
+            internal set { //used by the serializer
+                startPoint = value;
+            }
         }
-
+    
+        [DataMember]
         public VertexT EndPoint {
             get {
                 return endPoint;
             }
+            
+            internal set { //used by the serializer
+                endPoint = value;
+            }
         }
-
+        
+        [DataMember]
         public EdgeT Value {
             get {
                 return value;
