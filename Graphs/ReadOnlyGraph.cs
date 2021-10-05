baedf8001732ae3616a4bf0e08c1e9df87ef1c99
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-10-05 17:21:04 +0300

    Adjusted interface

--- Graphs/ReadOnlyGraph.cs
+++ Graphs/ReadOnlyGraph.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.ObjectModel;
 
 namespace GraphLibrary {
+
     /// <summary>
     /// A read-only wrapper for the graph. Users using this wrapper will have access to all the data within, but will 
     /// be unable to mutate them.<br></br>
@@ -11,15 +12,16 @@ namespace GraphLibrary {
     /// <typeparam name="VertexT">The type of objects stored in the graph's vertices.</typeparam>
     /// <typeparam name="EdgeT">The type of objects stored in the graph's edges.</typeparam>
     internal class ReadOnlyGraph<VertexT, EdgeT> : IGraph<VertexT, EdgeT> {
-        private static readonly String errorMessage = "This operation violates the READ-ONLY policy of this object.";
 
-        private readonly Graph<VertexT, EdgeT> actualGraph; 
+        private readonly IGraph<VertexT, EdgeT> actualGraph;
+
+        private static readonly String errorMessage = "This operation violates the READ-ONLY policy of this object.";
 
         /// <summary>
         /// Creates a new read-only wrapper for the graph. O(1) operation.
         /// </summary>
         /// <param name="graph">The graph to be used in the wrapper.</param>
-        internal ReadOnlyGraph(Graph<VertexT,EdgeT> graph) {
+        internal ReadOnlyGraph(IGraph<VertexT,EdgeT> graph) {
             actualGraph = graph;
         }
 
@@ -34,6 +36,10 @@ namespace GraphLibrary {
                 return actualGraph.IsDirected;
             }
         }
+
+        ReadOnlyCollection<VertexT> IGraph<VertexT, EdgeT>.Vertices => actualGraph.Vertices;
+
+        ReadOnlyCollection<Edge<VertexT, EdgeT>> IGraph<VertexT, EdgeT>.Edges => actualGraph.Edges;
 
         public void AddVertex(VertexT key) {
             throw new InvalidOperationException(errorMessage);
@@ -59,14 +65,6 @@ namespace GraphLibrary {
             throw new InvalidOperationException(errorMessage);
         }
 
-        public ReadOnlyCollection<VertexT> Vertices() {
-            return actualGraph.Vertices();
-        }
-
-        public ReadOnlyCollection<Edge<VertexT, EdgeT>> Edges() {
-            return actualGraph.Edges();
-        }
-
         public ReadOnlyCollection<Edge<VertexT, EdgeT>> IncidentEdges(VertexT key) {
             return actualGraph.IncidentEdges(key);
         }
@@ -77,6 +75,10 @@ namespace GraphLibrary {
 
         public bool AreAdjacent(VertexT obj1, VertexT obj2) {
             return actualGraph.AreAdjacent(obj1, obj2);
+        }
+
+        public Edge<VertexT, EdgeT> GetEdge(VertexT obj1, VertexT obj2) {
+            return actualGraph.GetEdge(obj1, obj2);
         }
     }
 }
